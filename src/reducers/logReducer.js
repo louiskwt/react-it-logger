71ec1ce2dc9db7f983d3e2177572ed5f71e6232f
Ka Wing Tsang <kawingt2@gmail.com>
2021-05-07 15:35:04 +0800

    Add and Delete Log

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types';
+import {
+	GET_LOGS,
+	ADD_LOG,
+	DELETE_LOG,
+	SET_LOADING,
+	LOGS_ERROR
+} from '../actions/types';
 import store from '../store';
 
 const initialState = {
@@ -14,6 +20,18 @@ export default (state = initialState, action) => {
 			return {
 				...state,
 				logs: action.payload,
+				loading: false
+			};
+		case ADD_LOG:
+			return {
+				...state,
+				logs: [...state.logs, action.payload],
+				loading: false
+			};
+		case DELETE_LOG:
+			return {
+				...state,
+				logs: state.logs.filter((log) => log.id !== action.payload),
 				loading: false
 			};
 		case SET_LOADING:
