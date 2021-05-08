813a5b8cd3f255cb2a008a3adb5d2cbaa843be02
Ka Wing Tsang <kawingt2@gmail.com>
2021-05-08 11:41:14 +0800

    Add techs and Delete techs

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -3,7 +3,8 @@ import {
 	ADD_TECH,
 	DELETE_TECH,
 	TECHS_ERROR,
-	SET_LOADING
+	SET_LOADING,
+	DELETE_LOG
 } from './types';
 
 // Get techs from server
@@ -17,6 +18,51 @@ export const getTechs = () => async (dispatch) => {
 		dispatch({
 			type: GET_TECHS,
 			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: err.response.statusText
+		});
+	}
+};
+
+// Add Tech
+export const addTech = (tech) => async (dispatch) => {
+	try {
+		setLoading();
+
+		const res = await fetch('/techs', {
+			method: 'POST',
+			body: JSON.stringify(tech),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const data = await res.json();
+
+		dispatch({
+			type: ADD_TECH,
+			payload: data
+		});
+	} catch (err) {
+		dispatch({
+			type: TECHS_ERROR,
+			payload: err.response.statusText
+		});
+	}
+};
+
+// Delete tech from server
+export const deleteTech = (id) => async (dispatch) => {
+	try {
+		setLoading();
+
+		await fetch(`/techs/${id}`, { method: 'DELETE' });
+
+		dispatch({
+			type: DELETE_TECH,
+			payload: id
 		});
 	} catch (err) {
 		dispatch({
--- src/components/techs/TechItems.js
+++ src/components/techs/TechItems.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { deleteTech } from '../../actions/techActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
-const TechItems = ({ tech }) => {
+const TechItems = ({ tech, deleteTech }) => {
+	const onDelete = () => {
+		deleteTech(tech.id);
+		M.toast({ html: `${tech.firstName} was deleted from technican` });
+	};
 	return (
 		<li className='collection-item'>
 			<div>
 				{tech.firstName} {tech.lastName}
 				<a href='#!' className='secondary-content'>
-					<i className='material-icons grey-text'>delete</i>
+					<i className='material-icons grey-text' onClick={onDelete}>
+						delete
+					</i>
 				</a>
 			</div>
 		</li>
@@ -15,7 +24,8 @@ const TechItems = ({ tech }) => {
 };
 
 TechItems.propTypes = {
-	tech: PropTypes.object.isRequired
+	tech: PropTypes.object.isRequired,
+	deleteTech: PropTypes.func.isRequired
 };
 
-export default TechItems;
+export default connect(null, { deleteTech })(TechItems);
